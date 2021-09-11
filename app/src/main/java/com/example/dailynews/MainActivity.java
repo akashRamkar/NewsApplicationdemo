ebe232d25f468c2f1cb4a23f477233a1bb026a63
AkashRamkar <akashrramkar2001@gmail.com>
2021-09-11 14:33:35 +0530

    splash screen implemented into main activity

--- app/src/main/java/com/example/dailynews/MainActivity.java
+++ app/src/main/java/com/example/dailynews/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.dailynews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+//this will be splash screen activity
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this,NewsActivity.class));
+                finish();
+            }
+        }, 2000);
     }
 }
